c18d11539971a6ac9c597e278aff013b2b1e0e75
hamzakamran <hamzakwebdev@gmail.com>
2024-06-11 11:22:45 -0500

    Role

--- src/api/user/validate.js
+++ src/api/user/validate.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const userValidationSchema = Joi.object({
 	email: Joi.string().required(),
 	password: Joi.string(),
-	role: Joi.string().valid("manager", "tenant"),
+	role: Joi.string().valid("Manager", "Owner", "Tenant"),
 	firstName: Joi.string(),
 	lastName: Joi.string(),
 	status: Joi.string().valid("Active", "Disabled"),
