b28eb54b265592a0417faf489555ab418559a47e
hamzakamran <hamzakwebdev@gmail.com>
2024-06-11 13:13:35 -0500

    Document api

--- src/api/document/model.js
+++ src/api/document/model.js
@@ -2,6 +2,10 @@ const { Schema, model } = require("mongoose");
 
 const documentSchema = new Schema(
 	{
+		property: {
+			type: Schema.Types.ObjectId,
+			ref: "Property",
+		},
 		file: { type: String, required: true },
 		type: { type: String, required: true },
 		description: { type: String },
--- src/api/document/validate.js
+++ src/api/document/validate.js
@@ -1,6 +1,7 @@
 const Joi = require("joi");
 
 const documentValidation = Joi.object({
+	property: Joi.string().required(),
 	file: Joi.string().required(),
 	type: Joi.string().required(),
 	description: Joi.string(),
