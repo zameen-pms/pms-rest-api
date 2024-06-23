ce186b3ae1b25c5dfcc6f14e4ccef4d72be52bce
hamzakamran <hamzakwebdev@gmail.com>
2024-06-23 11:36:14 -0500

    Application

--- src/api/application/controller.js
+++ src/api/application/controller.js
@@ -19,7 +19,7 @@ const createObject = async (req, res) => {
 
 const getObjects = async (req, res) => {
 	try {
-		const objects = await Application.find(req.query);
+		const objects = await Application.find(req.query).populate("user");
 		res.json(objects);
 	} catch (err) {
 		res.status(500).json(err.message);
