2aa0b6a777bad012c29578172f769567f2f67cfa
hamzakamran <hamzakwebdev@gmail.com>
2024-07-25 18:44:08 -0500

    CORS

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,10 +1,8 @@
 const allowedOrigins = [
 	"http://localhost:5001",
 	"http://localhost:5002",
-	"https://admin-zameen-management.netlify.app",
 	"https://zameen-management.com",
-	"https://zameen-management.netlify.app",
-	"*.zameen-management.com",
+	"https://manager.zameen-management.com",
 ];
 
 module.exports = allowedOrigins;
