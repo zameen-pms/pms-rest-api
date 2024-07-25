f92d09668aa20e4539cb245900b3d19a0b58232c
hamzakamran <hamzakwebdev@gmail.com>
2024-07-25 18:53:15 -0500

    CORS

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -3,6 +3,8 @@ const allowedOrigins = [
 	"http://localhost:5002",
 	"https://zameen-management.com",
 	"https://manager.zameen-management.com",
+	"https://tenant.zameen-management.com",
+	"https://owner.zameen-management.com",
 ];
 
 module.exports = allowedOrigins;
